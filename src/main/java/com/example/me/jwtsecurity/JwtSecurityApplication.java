6f516ab2757c4a8d7199d0289b10c9f92153fada
chenliang <1484157619@qq.com>
2019-11-28 11:21:12 +0800

    springboot整合mybatis完善1

--- src/main/java/com/example/me/jwtsecurity/JwtSecurityApplication.java
+++ src/main/java/com/example/me/jwtsecurity/JwtSecurityApplication.java
@@ -3,12 +3,12 @@ package com.example.me.jwtsecurity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-
-@MapperScan("com.example.me.jwtsecurity.mapper") //扫描的mapper
-@SpringBootApplication(scanBasePackages = {"com.example.me"})
-@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {com.example.me.jwtsecurity.mapper.UserMapper.class})})
+import org.springframework.context.annotation.ImportResource;
+//扫描的mapper
+@MapperScan("com.example.me.jwtsecurity.mapper")
+@SpringBootApplication/*(scanBasePackages = {"com.example.me"})
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {com.example.me.jwtsecurity.mapper.UserMapper.class})})*/
+@ImportResource(locations = {"classpath: **/*.xml","classpath: **/*.yml","classpath: **/*.properties"})
 public class JwtSecurityApplication {
 
     public static void main(String[] args) {
